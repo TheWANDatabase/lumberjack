30ed9df1f461fe3e48beb95fe742ae3027fcd28c
Altrius <tom.b.2k2@gmail.com>
2023-11-22 21:16:31 +0000

    updated build
    Signed-off-by: Altrius <tom.b.2k2@gmail.com>

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,21 +1,20 @@
 export default class Logger {
-  readonly source: string;
-  readonly version: string;
-  readonly metadata: any;
-  private remote;
+    readonly source: string;
+    readonly version: string;
+    readonly metadata: any;
+    private remote;
+    private writeToLog;
 
-  constructor(module: string, version: string, metadata?: any);
+    constructor(module: string, version: string, metadata?: any);
 
-  log(...args: any[]): Promise<bigint>;
+    log(...args: any[]): Promise<bigint>;
 
-  info(...args: any[]): Promise<bigint>;
+    info(...args: any[]): Promise<bigint>;
 
-  warn(...args: any[]): Promise<bigint>;
+    warn(...args: any[]): Promise<bigint>;
 
-  error(...args: any[]): Promise<bigint>;
+    error(...args: any[]): Promise<bigint>;
 
-  debug(...args: any[]): Promise<bigint>;
-
-  private writeToLog;
+    debug(...args: any[]): Promise<bigint>;
 }
 //# sourceMappingURL=index.d.ts.map
